e599f379e5c16f46618f68c4cba39bc9654d14a6
Jhale18 <hale2jc@mail.uc.edu>
2025-03-07 10:26:16 -0500

    Update ASS01_RockPaperScissor.java

--- src/ASS01_RockPaperScissor.java
+++ src/ASS01_RockPaperScissor.java
@@ -1,15 +1,44 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class ASS01_RockPaperScissor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String player1;
+        String player2;
+        String playAgain;
+        do {
+            do {
+                System.out.print("Player 1, enter your move (R for rock, P for paper, S for scissors): ");
+                player1 = scanner.nextLine().toUpperCase();
+                if (!player1.matches("[RPS]")) {
+                    System.out.println("Invalid choice");
+                }
+            } while (!player1.matches("[RPS]"));
+            do {
+                System.out.print("Player 2, enter your move (R for rock, P for paper, S for scissors): ");
+                player2 = scanner.nextLine().toUpperCase();
+                if (!player2.matches("[RPS]")) {
+                    System.out.println("Invalid choice");
+                }
+            } while (!player2.matches("[RPS]"));
+            if (player1.equals(player2)) {
+                System.out.println("It's a tie! Both players chose " + player1);
+            } else if (player1.equals("R") || player2.equals("S")) {
+                System.out.println("Rock breaks Scissors. Player 1 wins!");
+            } else if (player1.equals("P") || player2.equals("R")) {
+                System.out.println("Paper covers Rock. Player 1 wins!");
+            } else if (player1.equals("S") || player2.equals("P")) {
+                System.out.println("Scissors cuts Paper. Player 1 wins!");
+            } else if (player2.equals("R") || player1.equals("S")) {
+                System.out.println("Rock breaks Scissors. Player 2 wins!");
+            } else if (player2.equals("P") || player1.equals("R")) {
+                System.out.println("Paper covers Rock. Player 2 wins!");
+            } else if (player2.equals("S") || player1.equals("P")) {
+                System.out.println("Scissors cuts Paper. Player 2 wins!");
+            }
+            System.out.print("Play again (Y/N): ");
+            playAgain = scanner.nextLine().toUpperCase();
+        } while (playAgain.equals("Y"));
+        System.out.println("Thanks for playing!");
     }
 }
