b29d5b12966d030c1e30abcceae909d83328b1ef
Jhale18 <hale2jc@mail.uc.edu>
2025-03-07 10:35:00 -0500

    Update ASS01_RockPaperScissor.java

--- src/ASS01_RockPaperScissor.java
+++ src/ASS01_RockPaperScissor.java
@@ -11,14 +11,14 @@ public class ASS01_RockPaperScissor {
                 System.out.print("Player 1, enter your move (R for rock, P for paper, S for scissors): ");
                 player1 = scanner.nextLine().toUpperCase();
                 if (!player1.matches("[RPS]")) {
-                    System.out.println("Invalid choice");
+                    System.out.println("Error, invalid choice");
                 }
             } while (!player1.matches("[RPS]"));
             do {
                 System.out.print("Player 2, enter your move (R for rock, P for paper, S for scissors): ");
                 player2 = scanner.nextLine().toUpperCase();
                 if (!player2.matches("[RPS]")) {
-                    System.out.println("Invalid choice");
+                    System.out.println("Error, invalid choice");
                 }
             } while (!player2.matches("[RPS]"));
             if (player1.equals(player2)) {
@@ -36,7 +36,7 @@ public class ASS01_RockPaperScissor {
             } else if (player2.equals("S") || player1.equals("P")) {
                 System.out.println("Scissors cuts Paper. Player 2 wins!");
             }
-            System.out.print("Play again (Y/N): ");
+            System.out.print("Play again? (Y/N): ");
             playAgain = scanner.nextLine().toUpperCase();
         } while (playAgain.equals("Y"));
         System.out.println("Thanks for playing!");
